ff528e12146bb0825c69a3697cde0e60c14a7b15
Yerin Hwang <hdan3653@gmail.com>
2020-02-29 00:26:13 +0900

    Deleted temp files

---
+++ app/src/main/java/ewha/appsolute/linkeep/MainActivity.kt
@@ -0,0 +1,13 @@
+package ewha.appsolute.linkeep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //TEMP
+        setContentView(R.layout.activity_new_link)
+    }
+}
