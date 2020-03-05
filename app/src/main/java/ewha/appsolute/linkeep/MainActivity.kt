2d1713bc74db43939d11e9225f949090fe9d90f0
Yerin Hwang <hdan3653@gmail.com>
2020-03-06 02:49:06 +0900

    new link activity event

--- app/src/main/java/ewha/appsolute/linkeep/MainActivity.kt
+++ app/src/main/java/ewha/appsolute/linkeep/MainActivity.kt
@@ -3,7 +3,7 @@ package ewha.appsolute.linkeep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class `MainActivity.kt` : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
